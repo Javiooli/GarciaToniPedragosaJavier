e0165dfab9a3ae95fd65823b9a45fea65f46ad77
Javiooli <jpedralo7@alumnes.ub.edu>
2022-02-21 12:06:02 +0100

    Update Pista.java

--- GarciaToniPedragosaJavier/src/prog2/model/Pista.java
+++ GarciaToniPedragosaJavier/src/prog2/model/Pista.java
@@ -6,4 +6,47 @@ public class Pista {
     private float longitud;
     private String estatNeu;
     private String estatPista;
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getLongitud() {
+        return this.longitud;
+    }
+
+    public void setLongitud(float longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getEstatNeu() {
+        return this.estatNeu;
+    }
+
+    public void setEstatNeu(String estatNeu) {
+        this.estatNeu = estatNeu;
+    }
+
+    public String getEstatPista() {
+        return this.estatPista;
+    }
+
+    public void setEstatPista(String estatPista) {
+        this.estatPista = estatPista;
+    }
+
 }
+
+
