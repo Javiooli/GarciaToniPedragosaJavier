0366cf494ba63da1c21ddacd862be70fc22b62fe
Javiooli <jpedralo7@alumnes.ub.edu>
2022-02-21 11:59:43 +0100

    Create Pista.java

---
+++ GarciaToniPedragosaJavier/src/prog2/model/Pista.java
@@ -0,0 +1,9 @@
+package prog2.model;
+
+public class Pista {
+    private String nom;
+    private String color;
+    private float longitud;
+    private String estatNeu;
+    private String estatPista;
+}
